ef5cc18a65ff411b0736d32c94128bedaed98212
vijay gavali <vijaygawli37@gmail.com>
2025-09-14 19:38:51 +0530

    Added user registration page and logic

---
+++ src/main/java/com/controller/UserRegistration.java
@@ -0,0 +1,53 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.dbconnection.DBConnection;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserRegistration extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String contact_no = request.getParameter("contact_no");
+			String role = request.getParameter("role");
+			String age = request.getParameter("age");
+			String grade = request.getParameter("grade");
+			String class_id = request.getParameter("class_id");
+
+			Connection connection = DBConnection.getConnection();
+
+            String insertQuery = "INSERT INTO users (name, email, password, contact_no, role, age, grade, class_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = connection.prepareStatement(insertQuery);
+            
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, password);
+            ps.setString(4, contact_no);
+            ps.setString(5, role);
+            ps.setString(6, age);
+            ps.setString(7, grade);
+            ps.setString(8, class_id);
+            
+            ps.executeUpdate();
+            connection.close();
+			
+			PrintWriter out = response.getWriter();
+			out.println("<h3>User registered successfully!</h3>");
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
---
+++ src/main/webapp/userRegistration.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>User Registration</title>
+<link rel="stylesheet" type="text/css" href="css/userRegistration.css">
+</head>
+<body>
+
+	<h2>User Registration</h2>
+	<form action="UserRegistration" method="post">
+		<label>Name:</label>
+		<input type="text" name="name" required /><br />
+		
+		<label>Email:</label>
+		<input type="email" name="email" required /><br />
+		
+		<label>Password:</label>
+		<input type="password" name="password" required /><br /> 
+			
+			<label>Contact Number:</label>
+			<input type="text" name="contact_no" required /><br /> 
+			
+			<label>Role:</label> 
+			<select name="role" required>
+			<option value="Teacher">Teacher</option>
+			<option value="Parent">Parent</option>
+			<option value="Student">Student</option>
+		</select><br /> 
+		
+		<label>Age (only for Student):</label> 
+		<input type="number" name="age" /><br /> 
+		
+		<label>Grade (only for Student):</label>
+		<input type="text" name="grade" /><br />
+		
+		<label>Select Class(only for Student):</label> 
+		<select name="Class_Standard" required>
+			<option value="1st_standard">1st standard</option>
+			<option value="2nd_standard">2nd standard</option>
+			<option value="3rd_standard">3rd standard</option>
+			<option value="4th_standard">4th standard</option>
+			<option value="5th_standard">5th standard</option>
+			<option value="6th_standard">6th standard</option>
+			<option value="7th_standard">7th standard</option>
+			<option value="8th_standard">8th standard</option>
+			<option value="9th_standard">9th standard</option>
+			<option value="10th_standard">10th standard</option>
+		</select><br />  
+		
+		<input type="submit" value="Add User" />
+	</form>
+</body>
+</html>
